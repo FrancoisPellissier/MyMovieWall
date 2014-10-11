9dbb06a84aa79d0e84580eec3fec8ae0e75e1247
FrancoisPellissier <f_pellissier@hotmail.com>
2014-10-11 18:54:51 +0200

    Pavé infos persos sur le film

--- modules/Film/view/show.tpl.php
+++ modules/Film/view/show.tpl.php
@@ -2,10 +2,16 @@
     <div class="col-xs-4 col-sm-4 col-md-3">
     	<?php echo '<img src="'.$curFiche['folder'].$curFiche['movieid'].'.jpg" alt="Affiche du film" title="'.$curFiche['titrevf'].'" class="img-rounded" />'; ?>
     </div>
-    <div class="col-xs-8 col-sm-8 col-md-9">
+    <div class="col-xs-8 col-sm-8 col-md-7 filminfos">
     	<h1><?php echo $curFiche['titrevf']; ?></h1>
         
-        <p>
+        <p><strong>Date de sortie :</strong> <?php echo $curFiche['datesortie']; ?></p>
+    	<p><strong>Réalisateur :</strong> <?php echo $curFiche['realisateur']; ?></p>
+    	<p><strong>Acteurs principaux :</strong> <?php echo $curFiche['acteur']; ?></p>
+    	<p><strong>Synopsis :</strong> <?php echo $curFiche['synopsis']; ?></p>
+    </div>
+    <div class="col-md-2">
+        <p>Vous le possédez en :<br />
             <?php
             foreach(array('bluray' => 'Blu-Ray', 'dvd' => 'DVD') AS $type => $typename) {
                 echo "\n\t\t\t".'<a title="'.$type.'" href="film/'.$curFiche['movieid'].'/addBiblio/'.$type.'" class="addBiblio"><button type="button" class="btn '.($user['hasFilm'][$type] == 1 ? 'btn-success' : 'btn-default').'" id="'.$type.'">'.$typename.'</button></a>';
@@ -14,27 +20,25 @@
         </p>
         <?php
         if(empty($user['hasViewFilm']))
-            echo '<p>Vous n\'avez jamais vu ce film</p>';
+            echo '<p>Vous ne l\'avez jamais vu</p>';
         else {
-            echo "\n\t\t".'<p>Vous avez vu ce film ';
+            echo "\n\t\t".'<p>Vous l\'avez vu :';
 
             foreach($user['hasViewFilm'] AS $id => $vue) {
-                echo ($id != 0 ? ', ' : '').'le '.$vue['viewdate'].($vue['type'] == '1' ? ' au cinéma' : ' à la télé');
+                echo '<br />- le '.$vue['viewdate'].($vue['type'] == '1' ? ' au cinéma' : ' à la télé');
             }
-            echo '.</p>';
+            echo '</p>';
         }
         
         ?>
         <form action="film/<?php echo $curFiche['movieid']; ?>/addView" method="post">
             <p>
-                <label><input type="radio" name="type" value="1" />Cinéma</label> <label><input type="radio" name="type" value="2" /> Télé</label> <input type="date" name="viewdate" id="viewdate" /> <input type="submit" name="addView" value="Enregistrer" /></p>
+                <label><input type="radio" name="type" value="1" />Cinéma</label> <label><input type="radio" name="type" value="2" /> Télé</label>
+                <br /><input type="date" name="viewdate" id="viewdate" />
+                <br /><input type="submit" name="addView" value="Enregistrer" /></p>
         </form>
-    	
-        <p><strong>Date de sortie :</strong> <?php echo $curFiche['datesortie']; ?></p>
-    	<p><strong>Réalisateur :</strong> <?php echo $curFiche['realisateur']; ?></p>
-    	<p><strong>Acteurs principaux :</strong> <?php echo $curFiche['acteur']; ?></p>
-    	<p><strong>Synopsis :</strong> <?php echo $curFiche['synopsis']; ?></p>
     </div>
+
 </div>
 
 <div class="row">
