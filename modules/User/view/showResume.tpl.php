0bc9adbc185d3dc6cf594ecce9dbfe975e2b52e9
FrancoisPellissier <f_pellissier@hotmail.com>
2014-11-17 12:31:51 +0100

    Correct tpl error

--- modules/User/view/showResume.tpl.php
+++ modules/User/view/showResume.tpl.php
@@ -20,5 +20,5 @@
 	foreach($lastBiblio AS $cur)
 		echo "\n\t".'<div class="col-xs-4 col-sm-3 col-md-2"><a href="film/'.$cur['movieid'].'"><img src="'.library\Image::getUrl('movie', $cur['movieid']).'" alt="Affiche du film" title="'.$cur['titrevf'].'" class="img-rounded"></a></div>';
 	?>
-<p><a href="user/<?php echo $curUser['id']; ?>/biblio">Voir la liste complète</a></p>
 </div>
+<p><a href="user/<?php echo $curUser['id']; ?>/biblio">Voir la liste complète</a></p>
