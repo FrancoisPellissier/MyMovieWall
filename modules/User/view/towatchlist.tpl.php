0379270dbf6f40d21c87fc1df558054db33f70da
FrancoisPellissier <f_pellissier@hotmail.com>
2015-01-21 19:39:03 +0100

    Enhance towatchlist : allready in theatre

--- modules/User/view/towatchlist.tpl.php
+++ modules/User/view/towatchlist.tpl.php
@@ -21,18 +21,26 @@ if($type == 2) {
 else {
 	$curdate = '';
 	foreach($films AS $film) {
-		
-		// On change de semaine
-		if($curdate != $film['datesortie']) {
-			// Si on n'itère pas pour la première fois
-			if($curdate != '')
-				echo "\n\t".'</div>';
-			
-			echo "\n\t".'<h3>'.library\Date::formatDate($film['datesortie'], 'J mois annee').'</h3>';
+		// Films déjà sortis
+		if($film['datesortie'] <= date('Y-m-d')) {			
+			if($curdate == '') {
+			echo "\n\t".'<h3>Déjà en salle</h3>';
 			echo "\n\t".'<div class="row">';
 			$curdate = $film['datesortie'];
+			}
 		}
-
+		else {
+			// On change de semaine
+			if($curdate != $film['datesortie']) {
+				// Si on n'itère pas pour la première fois
+				if($curdate != '')
+					echo "\n\t".'</div>';
+				
+				echo "\n\t".'<h3>'.library\Date::formatDate($film['datesortie'], 'J mois annee').'</h3>';
+				echo "\n\t".'<div class="row">';
+				$curdate = $film['datesortie'];
+			}
+		}
 		echo "\n\t".'<div class="col-xs-4 col-sm-3 col-md-2"><a href="film/'.$film['movieid'].'"><img src="'.library\Image::getUrl('movie', $film['movieid']).'" alt="Affiche du film" title="'.$film['titrevf'].'" class="img-rounded"></a></div>';
 	}
 	echo "\n\t".'</div>';	
