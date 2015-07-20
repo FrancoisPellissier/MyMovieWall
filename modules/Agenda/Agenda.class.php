371681727623799e80d2d48e8638a689796569c1
FrancoisPellissier <f_pellissier@hotmail.com>
2015-07-20 20:48:21 +0200

    Agenda: let the movie until sunday

--- modules/Agenda/Agenda.class.php
+++ modules/Agenda/Agenda.class.php
@@ -3,7 +3,7 @@ namespace modules\Agenda;
 
 class Agenda extends \library\BaseModel {
 	public function getFilms() {
-		$result = $this->db->query('SELECT * FROM movie WHERE datesortie > CURDATE() ORDER BY datesortie, titrevf, titrevo');
+		$result = $this->db->query('SELECT * FROM movie WHERE datesortie >= ADDDATE(CURDATE(), INTERVAL -4 DAY) ORDER BY datesortie, titrevf, titrevo');
 
 	    $last = array();
 	    while($cur = $this->db->fetch_assoc($result)) {
