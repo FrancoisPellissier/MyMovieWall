68de529e24a590fd283662f58da7b746b1c72e66
FrancoisPellissier <f_pellissier@hotmail.com>
2016-05-29 13:51:09 +0200

    Add new friend email template

---
+++ assets/mail_template/friend_ask.tpl.php
@@ -0,0 +1,9 @@
+Subject: <user_name> a validé votre demande
+
+Bonjour,
+
+L'utilisateur <user_name> a validé votre demande, il fait dorénavant partie de vos amis (et inversement).
+
+-- 
+<board_mailer>
+(Veuillez ne pas répondre à ce message. Merci !)
